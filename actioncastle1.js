0085bdbab6034d1e9707be353a947b690b45538f
M. Jones <miriam.j.jones.work@gmail.com>
2026-06-18 11:35:54 -0500

    6.18.26 11.35am ironed out some basic needs for input and decided on images instead of text for ascii

--- actioncastle1.js
+++ actioncastle1.js
@@ -1,4 +1,8 @@
+let charname = "";
 let inventory = [];
+let ytextInput = document.querySelector("#textInput");
+let textImage = document.querySelector("#textImage");
+let whichImage = "assets/snake.txt"; 
 
 //#region Locations
 function Cottage() {
@@ -8,4 +12,23 @@ function Cottage() {
 
     }
 };
+//#endregion
+
+//#region Basic Workings
+ytextInput.addEventListener("focus", function() {
+    ytextInput.select();
+});
+
+ytextInput.addEventListener("keypress", function(event) {
+    // Submit is permanent
+    if (event.code === "Enter") {
+        if (!((ytextInput.value).includes("Name here")) && !((ytextInput.value).includes("Fill this out please."))) {
+            ytextInput.setAttribute("disabled","disabled");
+            charname=ytextInput.value();
+            (document.querySelector("p")).innerHTML=charname;
+        } else  {
+            ytextInput.value="Fill this out please.";
+        }
+    }
+});
 //#endregion
