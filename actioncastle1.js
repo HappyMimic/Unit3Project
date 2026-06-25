08c287bc1bd1096d269f1235e272ddacaea2cfe9
M. Jones <miriam.j.jones.work@gmail.com>
2026-06-25 13:34:27 -0500

    6.1.34pm Finished the important part of troll encounter

--- actioncastle1.js
+++ actioncastle1.js
@@ -4,6 +4,8 @@
         let saving = "";
         let inventory = ["lamp"];
         let leftinv = [];
+        let troll = true;
+        let user = "";
     //#endregion
 
     //#region Element Definition
@@ -11,6 +13,7 @@
             let navbar = document.querySelector(".navbar");
             let homeButton = document.querySelector("#homeButton");
             let adjustPage = document.querySelector("#adjustPage");
+            let body = document.querySelector("body");
         //#endregion
 
         //#region Location
@@ -48,6 +51,8 @@
                 let binaryc = document.querySelector(".binary-choice");
                 let binaryA = document.querySelector("#optA");
                 let binaryB = document.querySelector("#optB");
+                let binaryC = document.querySelector("#optC");
+                let binaryD = document.querySelector("#optD");
             //#endregion
 
             //#region Text Input
@@ -57,9 +62,7 @@
         //#endregion
 
         let textImage = document.querySelector("#textImage");
-        console.log(textImage);
         let buttons = document.querySelectorAll("button:not(#homeButton,#adjustPage)");
-        console.log(buttons);
     //#endregion
 //#endregion
 
@@ -67,13 +70,24 @@
 
     //#region Resize
         function AdjustImage() {
-            let navbarheight = document.querySelector(".navbar").clientHeight;
+            let navbarheight = navbar.clientHeight;
             let imgwidth = textImage.width;
             // Image location
-            textImage.style.position="absolute";
+            textImage.style.position="fixed";
+            navbar.style.zIndex = "10";
+            textImage.style.zIndex="5";
+            locationm.style.zIndex="2";
+            navbar.style.width="100vw";
+            navbar.style.position="fixed";
+            interaction.style.overflowY="scroll";
+            locationm.style.overflowY="scroll";
+            locationm.style.overflowX="hidden";
+            interaction.style.overflowX="hidden";
             textImage.style.top = navbarheight+"px";
+            body.style.maxWidth = "100vw";
+            body.style.width = "100vw";
             textImage.style.left="0px";
-            locationm.style.position="absolute";
+            locationm.style.position="fixed";
             locationm.style.top = getComputedStyle(textImage).top;
             locationm.style.left=imgwidth+"px";
             locationm.style.width="calc(100vw - "+imgwidth+"px)";
@@ -98,7 +112,7 @@
     //#endregion
 
     //#region Repeat offenders
-        function getDialog(path) {
+        function getDialog(path, _callback = function(){}) {
             let txt;
             fetch("assets/text/"+path+".txt")
                 .then((res) => res.text())
@@ -106,6 +120,7 @@
                     dialog.innerHTML = text;
                 })
                 .catch((e) => console.error(e));
+            _callback();
         }
 
         function DisableAll() {
@@ -126,6 +141,9 @@
                 button.style.display = "none";
             });
             binaryc.style.display = "inline";
+            binaryC.style.display = "none";
+            binaryD.style.display = "none";
+            saving = "";
             dcontinue.disabled = "disabled";
         }
 
@@ -135,9 +153,13 @@
             if (index !== -1) {
                 list.splice(index, 1);
             }
-            
-            console.log(`Updated Array: ${list}`);
             return list;
+        }
+
+        function StartItAll() {
+            Cottage();
+            DisableAll();
+            dcontinue.disabled = "disabled";
         }
     //#endregion
 
@@ -150,15 +172,18 @@
             // Submit is permanent
             if (event.code === "Enter") {
                 if (!((ytextInput.value).includes("Name here")) && !((ytextInput.value).includes("Fill this out please."))) {
-                    ytextInput.setAttribute("disabled","disabled");
-                    charname=ytextInput.value();
-                    (document.querySelector("p")).innerHTML=charname;
+                    setTimeout(Input(),500);                
                 } else  {
                     ytextInput.value="Fill this out please.";
                 }
             }
         });
 
+        function Input() {
+            Cottage();
+            user = ytextInput.value;
+            ytextInput.disabled = "disabled";
+        }
 
     //#endregion
 
@@ -174,14 +199,13 @@
         specialSingle.disabled = "";
         specialSingle.style.display = "inline";
         specialSingle.innerHTML = "Out";
-
         NormalButtons();
         west.disabled="disabled";
         north.disabled="disabled";
         south.disabled="disabled";
         east.disabled="disabled";
         where = "cottage";
-        inputText.style.display = "none";
+        inputText.style.display = "auto";
         listc.style.display = "none";
 
         textImage.src = "assets/images/cabin.png";
@@ -206,25 +230,125 @@
         where = "gardenPath";
         textImage.src = "assets/images/garden-path.png";
         AdjustImage();
-};
+    };
+
+    function FishingPond() {
+        getDialog("fishingPond");
+        NormalButtons();
+        specialSingle.style.display = "none";
+        west.disabled="disabled";
+        north.disabled="";
+        south.disabled="disabled";
+        east.disabled="disabled";
+        where = "fishingPond";
+        textImage.src = "assets/images/fishing-pond.png";
+        AdjustImage();
+    };
+
+    function BendInRoad() {
+        getDialog("bendInRoad");
+        NormalButtons();
+        west.disabled="disabled";
+        north.disabled="disabled";
+        south.disabled="";
+        east.disabled="";
+        specialSingle.innerHTML="Up";
+        where = "bendInRoad";
+        textImage.src = "assets/images/bend-in-road.png";
+        AdjustImage();
+    };
+
+    function ToweringTree() {
+        getDialog("toweringTree");
+        NormalButtons();
+        specialSingle.style.display = "inline";
+        specialSingle.innerHTML = "Down";
+        west.disabled="disabled";
+        north.disabled="disabled";
+        south.disabled="disabled";
+        east.disabled="disabled";
+        where = "toweringTree";
+        textImage.src = "assets/images/towering-tree.png";
+        AdjustImage();
+    }
+
+    function Drawbridge() {
+        NormalButtons();
+        west.disabled="";
+        north.disabled="disabled";
+        south.disabled="disabled";
+        east.disabled="";
+        specialSingle.style.display="none";
+        textImage.src = "assets/images/drawbridge.png";
+        if (troll) {
+            getDialog("drawbridge");
+            DisableAll();
+        } else {
+            getDialog("drawbridge", function(){
+                console.log(dialog.innerHTML);
+            });
+        }
+        where = "drawbridge";
+        AdjustImage();
+    }
 //#endregion
 
 //#region Reactions
-    //#region Find Item
-    function FindRose() {
-        getDialog("findRose");
-        textImage.src = "assets/images/rose.png";
-        inventory.push("rose");
-        DisableAll();
+    //#region Other Goobers
+    function UselessHere(item) {
+        getDialog("uselessHere");
     }
 
-    function FindFishingPole() {
-        getDialog("findFishingPole");
-        textImage.src = "assets/images/fishing-pole.png";
-        inventory.push("fishing pole");
+    function RunInventory() {
+        if (leftinv.indexOf("lamp") === 0) {
+            getDialog("lampDesc");
+            textImage.src = "assets/images/lamp.png";
+            leftinv = Remove("lamp", leftinv);
+        } else if (leftinv.indexOf("fishingPole")===0) {
+            getDialog("fishingPoleDesc");
+            textImage.src = "assets/images/fishing-pole.png";
+            leftinv = Remove("fishingPole", leftinv);
+        } else if (leftinv.indexOf("rose")===0) {
+            getDialog("roseDesc");
+            textImage.src = "assets/images/rose.png";
+            leftinv = Remove("rose", leftinv);
+        } else if (leftinv.indexOf("branch")===0) {
+            getDialog("branchDesc");
+            textImage.src = "assets/images/branch.png";
+            leftinv = Remove("branch", leftinv);
+        } else if (leftinv.indexOf("fish")===0) {
+            getDialog("fishDesc");
+            textImage.src = "assets/images/fish.png";
+            leftinv = Remove("fish", leftinv);
+        } else if (leftinv.indexOf("key")===0) {
+            getDialog("keyDesc");
+            textImage.src = "assets/images/take-key.png";
+            leftinv = Remove("key", leftinv);
+        } else if (leftinv.indexOf("strangeCangle")===0) {
+            getDialog("strangeCandleDesc");
+            textImage.src = "assets/images/strange-cangle.png";
+            leftinv = Remove("strangeCandle", leftinv);
+        } else if (leftinv.indexOf("litLamp")===0) {
+            getDialog("litLampDesc");
+            textImage.src = "assets/images/light-lamp.png";
+            leftinv = Remove("litLamp", leftinv);
+        } else if (leftinv.indexOf("litStrangeCandle")===0) {
+            getDialog("litStrangeCandleDesc");
+            textImage.src = "assets/images/light-strange-candle.png";
+            leftinv = Remove("litStrangeCandle", leftinv);
+        }  else if (leftinv.indexOf("crown")===0) {
+            getDialog("crownDesc");
+            textImage.src = "assets/images/crown.png";
+            leftinv = Remove("crown", leftinv);
+        }
         DisableAll();
+        binaryC.style.display="inline";
+        binaryC.disabled="";
+        if (leftinv.length === 0) {
+            where = saving;
+            leftinv = [];
+        }
     }
-    //#endregion
 
     function NothingHere() {
         getDialog("nothingHere");
@@ -233,79 +357,154 @@
         DisableAll();
     }
 
-    function RunInventory() {
-        console.log("RUN INVENTORY:");
-        if (leftinv.includes("lamp")) {
-            getDialog("lampDesc");
-            textImage.src = "assets/images/lamp.png";
-            leftinv = Remove("lamp", leftinv);
-            console.log("   LAMP");
-        } else if (leftinv.includes("fishing pole")) {
-            getDialog("fishingPoleDesc");
-            textImage.src = "assets/images/fishing-pole.png";
-            leftinv = Remove("fishing pole", leftinv);
-            console.log("   FISHING POLE");
-        } else if (leftinv.includes("rose")) {
-            getDialog("roseDesc");
-            textImage.src = "assets/images/rose.png";
-            leftinv = Remove("rose", leftinv);
-            console.log("   ROSE");
-        } else if (leftinv.includes("branch")) {
-            getDialog("branchDesc");
-            textImage.src = "assets/images/branch.png";
-            leftinv = Remove("branch", leftinv);
-            console.log("   BRANCH");
-        } else if (leftinv.includes("fish")) {
-            getDialog("fishDesc");
-            textImage.src = "assets/images/fish.png";
-            leftinv = Remove("fish", leftinv);
-            console.log("   FISH");
-        } else if (leftinv.includes("key")) {
-            getDialog("keyDesc");
-            textImage.src = "assets/images/take-key.png";
-            leftinv = Remove("key", leftinv);
-            console.log("   KEY");
-        } else if (leftinv.includes("strange cangle")) {
-            getDialog("strangeCandleDesc");
-            textImage.src = "assets/images/strange-cangle.png";
-            leftinv = Remove("strange candle", leftinv);
-            console.log("   STRANGE CANDLE");
-        } else if (leftinv.includes("lit lamp")) {
-            getDialog("litLampDesc");
-            textImage.src = "assets/images/light-lamp.png";
-            leftinv = Remove("lit lamp", leftinv);
-            console.log("   LIT LAMP");
-        } else if (leftinv.includes("lit strange candle")) {
-            getDialog("litStrangeCandleDesc");
-            textImage.src = "assets/images/light-strange-candle.png";
-            leftinv = Remove("lit strange candle", leftinv);
-            console.log("   LIT STRANGE CANDLE");
-        }  else if (leftinv.includes("crown")) {
-            getDialog("crownDesc");
-            textImage.src = "assets/images/crown.png";
-            leftinv = Remove("crown", leftinv);
-            console.log("   CROWN");
-        }
-        if (leftinv.length === 0) {
-            where = saving;
-            saving = "";
-            leftinv = [];
-        }
+    function Troll() {
+            getDialog("troll");
+            NormalButtons();
+            west.disabled="";
+            north.disabled="disabled";
+            south.disabled="disabled";
+            east.disabled="disabled";
+            specialSingle.style.display="none";
+            textImage.src = "assets/images/troll.png";
+    }
+    //#endregion
+    
+    //#region Find Item
+    function FindFishingPole() {
+        getDialog("findFishingPole");
+        textImage.src = "assets/images/find-fishing-pole.png";
+        inventory.push("fishingPole");
         DisableAll();
     }
+
+    function FindRose() {
+        getDialog("findRose");
+        textImage.src = "assets/images/find-rose.png";
+        inventory.push("rose");
+        DisableAll();
+    }
+
+    function FindBranch() {
+        getDialog("findBranch");
+        textImage.src = "assets/images/find-branch.png";
+        inventory.push("branch");
+        DisableAll();
+    }
+    //#endregion
+
+    //#region Use Item
+        function GoFish() {
+            getDialog("goFish");
+            DisableAll();
+            textImage.src = "assets/images/go-fish.png";
+            binaryC.style.display="none";
+            where = "fishingPond";
+            inventory.push("fish");
+            AdjustImage();
+        }
+
+        function GiveTrollFish() {
+            getDialog("giveTrollFish");
+            DisableAll();
+            troll = false;
+            textImage.src = "assets/images/give-troll-fish.png";
+            binaryC.style.display="none";
+            where = "drawbridge";
+            inventory = Remove("fish",inventory);
+            AdjustImage();
+        }
+    //#endregion
 //#endregion
 
 //#region Button Events
     dcontinue.addEventListener("click", function() {
-        console.log("continuation");
         if (where === "cottage") {
             Cottage();
         } else if (where === "inventory") {
-            console.log("in the right area");
             RunInventory();
-            console.log("Afterfunction");
         } else if (where === "gardenPath") {
             GardenPath();
+        } else if (where === "fishingPond") {
+            FishingPond();
+        } else if (where === "bendInRoad") {
+            BendInRoad();
+        } else if (where === "toweringTree") {
+            ToweringTree();
+        } else if (where === "drawbridge" && troll) {
+            Troll();
+        } else if (where === "drawbridge" && !troll) {
+            Drawbridge();
+        } 
+    });
+
+    binaryC.addEventListener("click",function(){
+        let currentind = inventory.length - (leftinv.length + 1);
+        if (saving === "fishingPond" && inventory.indexOf("fishingPole") === currentind) {
+            GoFish();
+            leftinv = [];
+            inventory=Remove("fishingPole",inventory);
+        } else if (saving === "drawbridge" && inventory.indexOf("fish") === currentind && troll) {
+            GiveTrollFish();
+            leftinv = [];
+        } else if (saving === "drawbridge" && inventory.indexOf("branch") === currentind) {
+            DeathByTroll("branch");
+            leftinv = [];
+        } else if (saving === "dungeon" && inventory.indexOf("branch") === currentind) {
+            DeathByGhost("branch");
+            leftinv = [];
+        } else if (saving === "courtyardGuard" && inventory.indexOf("branch") === currentind) {
+            HitGuard();
+            leftinv = [];
+            inventory=Remove("branch",inventory);
+        } else if (saving === "princessRoom" && inventory.indexOf("rose") === currentind) {
+            GivePrincessRose();
+            leftinv = [];
+            inventory=Remove("rose",inventory);
+        } else if (saving === "dungeon" && inventory.indexOf("rose") === currentind) {
+            DeathByGhost("rose");
+            leftinv = [];
+        } else if (saving === "dungeon" && inventory.indexOf("strangeCandle") === currentind) {
+            LightStrangeCandle();
+            leftinv = [];
+        } else if (saving === "drawbridge" && inventory.indexOf("rose") === currentind) {
+            DeathByTroll("rose");
+            leftinv = [];
+        } else if (saving === "courtyardGuard" && !inventory.indexOf("") === currentind) {
+            ConfuseGuard();
+            leftinv = [];
+        } else {
+            UselessHere();
+        }
+    })
+
+    north.addEventListener("click", function(){
+        if (where === "fishingPond") {
+            GardenPath();
+        } else if (where === "gardenPath") {
+            BendInRoad();
+        }
+    });
+
+    south.addEventListener("click",function(){
+        if (where === "gardenPath") {
+            FishingPond();
+        } else if (where === "bendInRoad") {
+            GardenPath();
+        }
+    });
+
+    east.addEventListener("click",function(){
+        if (where === "bendInRoad") {
+            Drawbridge();
+        } else if (where === "drawbridge") {
+            alert("Thank you " + user + " for playing so far!\nThis game is yet to be complete, but come back later to learn how you become Ruler "+user+" of ACTION CASTLE (castle castle castle)!");
+            location.reload();
+        }
+    });
+
+    west.addEventListener("click",function(){
+        if (where === "drawbridge") {
+            BendInRoad();
         }
     });
 
@@ -314,31 +513,33 @@
             GardenPath();
         } else if (where === "gardenPath") {
             Cottage();
+        } else if (where === "bendInRoad") {
+            ToweringTree();
+        } else if (where === "toweringTree") {
+            BendInRoad();
         }
     });
 
     binaryA.addEventListener("click", function() {
-        if (where === "cottage") {
-            if (inventory.includes("fishing pole")) {
-                NothingHere();
-            } else {
-                FindFishingPole();
-            }
+        if (where === "cottage" && !inventory.includes("fishingPole")) {
+            FindFishingPole();
+        } else if (where === "gardenPath" && !inventory.includes("rose")) {
+            FindRose();
+        } else if (where === "toweringTree" && !inventory.includes("branch")) {
+            FindBranch();
+        } else {
+            NothingHere();
         }
     });
 
     binaryB.addEventListener("click", function() {
         saving = where;
         where = "inventory";
-        console.log("Just before:")
         leftinv = [...inventory];
-        console.log(leftinv);
-        console.log(inventory);
-        console.log(leftinv.length);
-        console.log("endreciept");
         RunInventory();
     });
 
 //#endregion
 
-GardenPath();
+Drawbridge();
+inventory.push("fish");
--- homepage.js
+++ homepage.js
@@ -1,5 +1,17 @@
-let goto = document.querySelector("#ac1button");
+let navbar = document.querySelector(".navbar");
+let ac1b = document.querySelector("#ac1button");
+let ac2b = document.querySelector("#ac2button");
+let ac3b = document.querySelector("#ac3button");
+let main = document.querySelector(".main");
 
-goto.addEventListener("click", function(){
+ac1b.addEventListener("click", function(){
     window.location.href = "actioncastle1.html";
+});
+
+ac2b.addEventListener("click", function(){
+    window.location.href = "actioncastle2.html";
+});
+
+ac3b.addEventListener("click", function(){
+    window.location.href = "actioncastle3.html";
 });
